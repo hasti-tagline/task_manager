7d68dcb3166474b969e674cddb13f85302b58dd2
hasti-tagline <hti@taglineinfotech.com>
2026-06-24 16:52:26 +0530

    add test case in account app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+
 
 # Create your tests here.
+from django.test import TestCase
+from accounts.models import User
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(
+            username="hasti",
+            password="1234"
+        )
+
+        self.assertEqual(user.username, "hasti")
