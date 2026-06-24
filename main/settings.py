47e6c17b969701234bdc2bb1e3e72955a0761283
hasti-tagline <hti@taglineinfotech.com>
2026-06-24 17:50:26 +0530

    update setting file

--- main/settings.py
+++ main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-API_KEY_SECRET = os.getenv("API_KEY_SECRET")
+API_KEY_SECRET = b'Je8-k936sCgOv5myhIDMU88XKwVsnwImU8uj006wNvE='
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
